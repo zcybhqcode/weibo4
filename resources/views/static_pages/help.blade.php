41acbe5d81de4d7dfb0341c727093f4f661c8712
zsc <2857625324.com>
2021-10-19 11:38:31 +0800

    基础页面

---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,17 @@
+<!-- <!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>weibo App</title>
+	</head>
+	<body>
+        <h1>帮助页</h1>
+	</body>
+</html>
+ -->
+
+ @extends('layouts.default')
+ @section('title', '帮助')
+ @section('content')
+  <h1>帮助页</h1>
+ @stop
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,16 @@
+<!-- <!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>weibo App</title>
+	</head>
+	<body>
+        <h1>主页</h1>
+	</body>
+</html> -->
+
+@extends('layouts.default')
+@section('title', '主页')
+@section('content')
+ <h1>主页</h1>
+@stop
