1bf7d75f87a64aba9b38bd4a3e00c56915c0604f
zsc <2857625324.com>
2021-10-21 14:56:37 +0800

    初始化样式

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,16 +1,16 @@
-<!-- <!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="utf-8">
-		<title>weibo App</title>
-	</head>
-	<body>
-        <h1>主页</h1>
-	</body>
-</html> -->
-
-@extends('layouts.default')
-@section('title', '主页')
-@section('content')
- <h1>主页</h1>
-@stop
+ @extends('layouts.default')
+ @section('content')
+ <div class="jumbotron">
+        <h1>Hello Laravel</h1>
+     <p class="lead">
+        你现在所看到的是
+        <a href="https://learnku.com/courses/laravel-essentialtraining">Laravel 入门教程</a> 的示例项目主页。
+     </p>
+     <p>
+        一切，将从这里开始。
+     </p>
+     <p>
+        <a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+     </p>
+ </div>
+ @stop
